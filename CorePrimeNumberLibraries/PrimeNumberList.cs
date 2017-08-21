a90a71ac98f72776e7888e4cbabdaa7cdaac4b15
Jon Spencer <jondspen@gmail.com>
2017-08-20 19:38:06 -0500

    Initial check in - Sieve of Eratosthenes Algorithm coded and working

---
+++ CorePrimeNumberLibraries/PrimeNumberList.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CorePrimeNumberLibraries
+{
+    public static class PrimeNumberList
+    { 
+        public static List<int> CreateListOfPrimes(int maxNumber)
+        {
+            List<int> primeList = new List<int>();
+
+            /* Creating a list of all prime numbers from 2 to maxNumber
+            * Using Sieve of Eratosthenes Algorithm
+            * https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+            * 
+            * "The sieve of Eratosthenes can be expressed in pseudocode, as follows"
+            *      Input: an integer n > 1.
+            *      Let A be an array of Boolean values, indexed by integers 2 to n,
+            *      initially all set to true.
+            *      
+            *      for i = 2, 3, 4, ..., not exceeding √n:
+            *          if A[i] is true:
+            *              for j = i^2, i^2+i, i^2+2i, i^2+3i, ..., not exceeding n:
+            *                  A[j] := false.
+            *      Output: all i such that A[i] is true.
+            *      
+            *      -------------------  Comments added below for each step of the algorithm  -------------------
+            *                           Algorithm is proposed to run in O(n * (log log n))
+            */
+
+            //  Input: an integer n > 1.
+            if (maxNumber > 1)
+            {
+                //Let A be an array of Boolean values, indexed by integers 2 to n, initially all set to true.
+                Dictionary <int, bool> numberDictionary = new Dictionary<int, bool>();
+
+                for (int keyValue = 2; keyValue <= maxNumber; keyValue++)
+                {
+                    numberDictionary.Add(keyValue, true);
+                }
+
+                //  for i = 2, 3, 4, ..., not exceeding √n:
+                for (int i = 2; i < Math.Sqrt(maxNumber); i++)
+                {
+                    // if A[i] is true:
+                    if (numberDictionary[i])
+                    {
+                        //  for j = i^2, i^2+i, i^2+2i, i^2+3i, ..., not exceeding n:
+                        int coefficient = 0;
+                        int squaredMultiple = MultiplesOfTheValue(i, coefficient);
+
+                        while (squaredMultiple <= maxNumber)
+                        {
+                            //  A[j] := false
+                            numberDictionary[squaredMultiple] = false;
+                            // ***** Iterate through i^2+(coefficient * i) by incrementing coefficient and evaluating the next squared multiple *****
+                            coefficient++;
+                            squaredMultiple = MultiplesOfTheValue(i, coefficient);
+                        }
+                    }
+                }
+
+                //  Output: all i such that A[i] is true
+                for (int dictionaryKey = 2; dictionaryKey <= maxNumber; dictionaryKey++)
+                {
+                    if (numberDictionary[dictionaryKey])
+                    {
+                        primeList.Add(dictionaryKey);
+                    }
+                }
+            }
+
+            return primeList;
+        }
+
+        private static int MultiplesOfTheValue(int keyValue, int coefficient)
+        {
+            return ((keyValue * keyValue) + (coefficient * keyValue));
+        }
+    }
+}
---
+++ CorePrimeNumbersConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using CorePrimeNumberLibraries;
+using System;
+
+namespace CorePrimeNumbersConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number N (greater than 1) to get a list of all primes <= N: ");
+            int maxNumber = 0;
+            if (int.TryParse(Console.ReadLine(), out maxNumber))
+            {
+                if (maxNumber > 1)
+                {
+                    var ListOfPrimes = PrimeNumberList.CreateListOfPrimes(maxNumber);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    for (int i = 0; i < ListOfPrimes.Count; i++)
+                    {
+                        Console.Write(ListOfPrimes[i].ToString() + " ");
+                    }
+
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.WriteLine("\nValue was not greater than 1.");
+                }                
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid number entered.");
+            }
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
